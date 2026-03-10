a8c4ee3f8cd38cadcab2b149a1cf857473be56c0
Phillip DuLion <phillip@dulion.com>
2026-03-10 15:25:27 -0700

    Initial layout with Kconfig

---
+++ components/network/include/dlt_network.h
@@ -0,0 +1,6 @@
+#ifndef DLT_NETWORK_H
+#define DLT_NETWORK_H
+
+// Your declarations here
+
+#endif // DLT_NETWORK_H
---
+++ components/provision/include/dlt_provision.h
@@ -0,0 +1,6 @@
+#ifndef DLT_PROVISION_H
+#define DLT_PROVISION_H
+
+// Your declarations here
+
+#endif // DLT_PROVISION_H
