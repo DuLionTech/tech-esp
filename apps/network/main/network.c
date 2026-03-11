1b3b05ee4876eb4908b61589ffde1d9956bc7592
Phillip DuLion <phillip@dulion.com>
2026-03-11 11:08:59 -0700

    With includes broken

--- apps/network/main/network.c
+++ apps/network/main/network.c
@@ -1,7 +1,34 @@
 #include <string.h>
-#include "dlt_network.h"
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
+#include <wifi_provisioning/manager.h>
+#include <wifi_provisioning/scheme_ble.h>
+#include <esp_netif.h>
+#include <esp_wifi.h>
+#include <nvs_flash.h>
 
-void app_main(void)
-{
+#include <dlt_network.h>
+#include "../../../components/provision/include/sec2.h"
 
+#define OK ESP_ERROR_CHECK
+
+static const char* TAG = "network";
+
+static EventGroupHandle_t wifi_event_group;
+
+void app_main(void) {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        OK(nvs_flash_erase());
+        OK(nvs_flash_init());
+    }
+
+    OK(esp_netif_init());
+    OK(esp_event_loop_create_default());
+
+    wifi_event_group = xEventGroupCreate();
+
+    // esp_netif_create_default_wifi_sta();
 }
--- components/provision/dlt_provision.c
+++ components/provision/dlt_provision.c
@@ -1,1 +1,35 @@
-#include "dlt_network.h"
+#include <string.h>
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
+#include <esp_netif.h>
+#include <esp_wifi.h>
+#include <wifi_provisioning/manager.h>
+#include <wifi_provisioning/scheme_ble.h>
+
+#include "dlt_provision.h"
+#define OK ESP_ERROR_CHECK
+
+// Private Declarations
+static void provision_handler(void* ctx, esp_event_base_t event_base, int32_t event_id, void* event_data);
+static void ble_handler(void* ctx, esp_event_base_t event_base, int32_t event_id, void* event_data);
+static void session_handler(void* ctx, esp_event_base_t event_base, int32_t event_id, void* event_data);
+
+// Public Implementations
+void create_wifi(void) {
+    OK(esp_event_handler_register(WIFI_PROV_EVENT, ESP_EVENT_ANY_ID, &provision_handler, NULL));
+    OK(esp_event_handler_register(PROTOCOMM_TRANSPORT_BLE_EVENT, ESP_EVENT_ANY_ID, &ble_handler, NULL));
+    OK(esp_event_handler_register(PROTOCOMM_SECURITY_SESSION_EVENT, ESP_EVENT_ANY_ID, &session_handler, NULL));
+}
+
+// Private Implementations
+static void provision_handler(void* ctx, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+}
+
+static void ble_handler(void* ctx, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+}
+
+static void session_handler(void* ctx, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+}
+
